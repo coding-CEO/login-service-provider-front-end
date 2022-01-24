0aad571eb5341f025ab9a768e1e5dcea228e2159
Pratik Raut <pratikraut0000@gmail.com>
2022-01-24 15:32:42 +0530

    installed axios and added basic utilities

---
+++ src/utils/axiosInstance.ts
@@ -0,0 +1,9 @@
+import axios from "axios";
+
+const baseOrigin = "http://127.0.0.1";
+
+export const normalAxiosInstance = axios.create({ baseURL: baseOrigin });
+export const credentialAxiosInstance = axios.create({
+  baseURL: baseOrigin,
+  withCredentials: true,
+});
