b9a4c3205af5cfb084237be07fb2761ca6aea0f1
Pratik Raut <pratikraut0000@gmail.com>
2022-01-27 20:51:52 +0530

    modified loginpage component and basic css

--- src/components/loginComponents/LoginPage.tsx
+++ src/components/loginComponents/LoginPage.tsx
@@ -1,9 +1,35 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 import './LoginPage.css';
 
+import googleIcon from '../../static/icons/google.svg';
+import facebookIcon from '../../static/icons/facebook.svg';
+import appleIcon from '../../static/icons/apple.svg';
+
+import OAuthProviderComponent from './oauthProvidersComponent/OAuthProvider';
+import { OAuthProvider } from '../../classes/oauthProvider';
+
 const LoginPage = () => {
+
+    const [oauthProviders, setOAuthProviders] = useState<OAuthProvider[]>([]);
+
+    useEffect(() => {
+        //TODO: add the apis below
+        setOAuthProviders([
+            new OAuthProvider("Facebook", facebookIcon, "", "#1877f2"),
+            new OAuthProvider("Google", googleIcon, "/googleGenerateUserOAuthCode", "#fff"),
+            new OAuthProvider("Apple", appleIcon, "", "#000"),
+        ]);
+    }, []);
+
     return (
-        <div>LoginPage</div>
+        <div className='loginPageContainer'>
+            <h2>Welcome to 2 click login</h2>
+            <div className="loginOAuthProvidersContainer">
+                {oauthProviders.map((oauthProvider: OAuthProvider) => {
+                    return <OAuthProviderComponent key={oauthProvider.name} oauthProvider={oauthProvider} />
+                })}
+            </div>
+        </div>
     );
 }
 
