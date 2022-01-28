579902ac4cbac70da955917400c439cc3f22cbb7
Pratik Raut <pratikraut0000@gmail.com>
2022-01-28 17:14:46 +0530

    added go to home page functionality in navbar title

--- src/components/navbarComponents/NavBar.tsx
+++ src/components/navbarComponents/NavBar.tsx
@@ -1,10 +1,12 @@
 import React from 'react';
+import { Page } from '../../utils/Page';
 import './NavBar.css';
 
 const NavBar = () => {
     return (
         <div className="navbarContainer">
-            <h3>Login Service Provider</h3>
+            <h3 style={{ cursor: "pointer" }}
+                onClick={() => Page.redirect(`/`)}>Login Service Provider</h3>
         </div>
     );
 }
